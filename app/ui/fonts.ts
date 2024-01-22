8ff9a1acebe6aa217dcf6a6c7505aa36ec985c9a
siavashrashno <100020679+siavashrashno@users.noreply.github.com>
2024-01-22 15:36:26 +0330

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusi = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
